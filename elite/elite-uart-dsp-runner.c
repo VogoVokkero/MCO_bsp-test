d391d069e2e470a7df58399e06809dbd70db897f
Marc Titinger <marc.titinger@vogo-groupe.com>
2023-01-27 10:05:07 +0100

    uart runner
    Signed-off-by: Marc Titinger <marc.titinger@vogo-groupe.com>

--- elite/elite-uart-dsp-runner.c
+++ elite/elite-uart-dsp-runner.c
@@ -5,23 +5,39 @@
  * See README
  */
 #include "esg-bsp-test.h"
+#include <termios.h>
 
 DLT_DECLARE_CONTEXT(dlt_ctxt_udsp);
+
+#define UART_STM "/dev/ttymxc1"
+
+int uart_fd;
 
 static void *elite_uart_dsp_runner(void *p_data)
 {
 	int ret = EXIT_SUCCESS;
 	uint32_t nb_loops = (uint32_t)p_data;
+	char line_buffer[4096];
 
-	int verbosity = (nb_loops < DLT_VERBOSITY_LOOP_THRESHOLD) ? DLT_LOG_VERBOSE : DLT_LOG_INFO ;
+	int verbosity = (nb_loops < DLT_VERBOSITY_LOOP_THRESHOLD) ? DLT_LOG_VERBOSE : DLT_LOG_INFO;
 
 	DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("START"));
 
 	while (0 < nb_loops--)
 	{
-		int avail;
+		char c;
+		uint16_t cur_pos = 0;
 
-		DLT_LOG(dlt_ctxt_udsp, verbosity, DLT_STRING("udsp_runner"), DLT_UINT32(nb_loops));
+		while (('\n' != c) && (512 > cur_pos))
+		{
+			ssize_t bytes = read(uart_fd, &c, sizeof(c));
+			if (sizeof(char) == bytes)
+			{
+				line_buffer[cur_pos++] = c;
+			}
+		}
+
+		DLT_LOG(dlt_ctxt_udsp, verbosity, DLT_STRING("udsp_runner"), DLT_STRING(line_buffer));
 	}
 
 	DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("EXIT"));
@@ -32,19 +48,77 @@ static void *elite_uart_dsp_runner(void *p_data)
 int elite_uart_dsp_init(pthread_t *runner, uint32_t nb_loops)
 {
 	int ret = EXIT_SUCCESS;
+	struct termios tty;
 
-	DLT_REGISTER_CONTEXT_LL_TS(dlt_ctxt_udsp,"UDSP","ESG BSP ELITE UART DSP Context", DLT_LOG_INFO, DLT_TRACE_STATUS_DEFAULT);
+	DLT_REGISTER_CONTEXT_LL_TS(dlt_ctxt_udsp, "UDSP", "ESG BSP ELITE UART DSP Context", DLT_LOG_INFO, DLT_TRACE_STATUS_DEFAULT);
 
-	DLT_LOG(dlt_ctxt_udsp, DLT_LOG_INFO, DLT_STRING("udsp_init: Using "), DLT_STRING((SAMPLE_ACCESS == SND_PCM_ACCESS_RW_NONINTERLEAVED) ? "non-interleaved" : "interleaved"));
+	DLT_LOG(dlt_ctxt_udsp, DLT_LOG_INFO, DLT_STRING("udsp_init"));
 
-	ret = pthread_create(runner, NULL, elite_uart_dsp_runner, nb_loops);
+	uart_fd = open(UART_STM, O_RDWR | O_NOCTTY | O_SYNC | O_NONBLOCK);
+	if (uart_fd < 0)
+	{
+		DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init: open failed " UART_STM));
+		ret = uart_fd;
+	}
+
+	if (EXIT_SUCCESS == ret)
+	{
+		ret = tcgetattr(uart_fd, &tty);
+		if (EXIT_SUCCESS != ret)
+		{
+			DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init: tcgetattr failed"));
+		}
+	}
+
+	if (EXIT_SUCCESS == ret)
+	{
+		cfsetospeed(&tty, (speed_t)B115200);
+		cfsetispeed(&tty, (speed_t)B115200);
+
+		tty.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
+		tty.c_cflag &= ~CSIZE;
+		tty.c_cflag |= CS8;		 /* 8-bit characters */
+		tty.c_cflag &= ~PARENB;	 /* no parity bit */
+		tty.c_cflag &= ~CSTOPB;	 /* only need 1 stop bit */
+		tty.c_cflag &= ~CRTSCTS; /* no hardware flowcontrol */
+
+		/* setup for non-canonical mode */
+		tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+		tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+		tty.c_oflag &= ~OPOST;
+
+		/* fetch bytes as they become available */
+		tty.c_cc[VMIN] = 1;
+		tty.c_cc[VTIME] = 1;
+
+		ret = tcsetattr(uart_fd, TCSANOW, &tty);
+
+		if (EXIT_SUCCESS != ret)
+		{
+			DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init: tcgetattr failed"));
+		}
+	}
+
+	if (EXIT_SUCCESS == ret)
+	{
+		char buff[200];
+		while (read(uart_fd, &buff, 200) > 0)
+		{
+			// printf("flushing...\n");
+		}
+
+		ret = pthread_create(runner, NULL, elite_uart_dsp_runner, nb_loops);
+
+		if (EXIT_SUCCESS != ret)
+		{
+			DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init: failed to creating runner"));
+		}
+	}
 
 	if (EXIT_SUCCESS != ret)
 	{
-		DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init: failed to creating running"));
+		DLT_LOG(dlt_ctxt_udsp, DLT_LOG_ERROR, DLT_STRING("udsp_init failed"));
 	}
-
-	/**/
 
 	return ret;
 }
